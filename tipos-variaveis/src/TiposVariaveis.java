fef830e8115b00ee49d5084f021181665420cc76
Jony Dias <jony.dias70@gmail.com>
2026-05-30 13:01:04 -0400

    Inclui valor do PI como constante e imprime na tela.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Tipos de Variáveis em Java:");
+        final double VALOR_PI = 3.14; // Constante, não pode ser alterada
+        System.out.println("Valor de PI: " + VALOR_PI);
     }
 }
