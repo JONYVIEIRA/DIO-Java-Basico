f8f86a420586caf51ed49616be3e79f642343e5a
Jony Dias <jony.dias70@gmail.com>
2026-05-30 11:51:06 -0400

    alteração 01

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,5 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
         System.out.println("Tipos de Variáveis em Java:");
-=======
-        System.out.println("Hello, World!");
->>>>>>> a25a87bd8944715f0fb2def38d31ba883f5c9cb7
     }
 }
